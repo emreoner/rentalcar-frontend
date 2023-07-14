40d4eb8f4dcbf195b76b5a8f1fdc74bc463facd9
EMRE ONER <EMREONER>
2023-07-14 15:31:15 +0300

    ListResponseModel Implementation

--- src/app/services/color.service.ts
+++ src/app/services/color.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ColorResponseModel } from '../models/colorResponseModel';
 import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/listResponseModel';
+import { Color } from '../models/color';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +12,7 @@ export class ColorService {
   apiUrl = 'https://localhost:44359/api/colors/getall';
   constructor(private httpClient: HttpClient) {}
 
-  getColors():Observable<ColorResponseModel> {
-    return this.httpClient.get<ColorResponseModel>(this.apiUrl);
+  getColors():Observable<ListResponseModel<Color>> {
+    return this.httpClient.get<ListResponseModel<Color>>(this.apiUrl);
   }
 }
